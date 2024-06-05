90b645e4c4e6193ba007fabf7fb599b537245f60
devellernmr <devellernmr@gmail.com>
2024-06-05 09:13:46 -0300

    Atualizar o

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', () => {
+        menuToggle.classList.toggle('active');
+        navLinks.classList.toggle('active');
+    });
+});
